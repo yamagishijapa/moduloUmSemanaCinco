95fcadca2111f9c2bb0fda17b26ec6e1196945f7
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-02-12 01:09:19 -0300

    [M1S05] EX 4 - Ranking de Jogadores  - Adicionado método para a exibição da listagem dos melhores jogadores e da posição do jogador na listagem.

--- src/service/JogoService.java
+++ src/service/JogoService.java
@@ -10,15 +10,37 @@ public class JogoService {
 
     List<Jogador> listaMelhoresJogadores = new ArrayList<>();
 
-    public void iniciaJogo(){
-        Jogador jogador =  new Jogador("Thiago", 26, 0, 5);
+    public void iniciaJogo() {
+        Jogador jogador = new Jogador("Thiago", 26, 100, 5);
         System.out.println("\nJogo iniciado");
         System.out.println("\nJogador: " + jogador);
         adicionarMelhorJogador(jogador);
+        exibeRanking(jogador);
     }
 
     public void adicionarMelhorJogador(Jogador jogador) {
         listaMelhoresJogadores.add(jogador);
         listaMelhoresJogadores.sort(Comparator.comparingInt(Jogador::getPontuacao).reversed());
+    }
+
+    public void exibeRanking(Jogador jogador) {
+        int posJogadorAtual = 0;
+        if (listaMelhoresJogadores.size() >= 10) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(listaMelhoresJogadores.get(i).getNome() + " - " + (i + 1));
+            }
+        } else {
+            for (int i = 0; i < listaMelhoresJogadores.size(); i++) {
+                System.out.println(listaMelhoresJogadores.get(i).getNome() + " - " + (i + 1));
+            }
+        }
+
+        for (int i = 0; i < listaMelhoresJogadores.size(); i++) {
+            if (listaMelhoresJogadores.get(i).equals(jogador)) {
+                posJogadorAtual = i + 1;
+            }
+        }
+
+        System.out.println("\nSua colocação: " + posJogadorAtual);
     }
 }
