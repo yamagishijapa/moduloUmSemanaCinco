254e8eac42ad6fafe769748cfc00a81c8f1f330a
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-02-12 00:27:20 -0300

    [M1S05] EX 2 - Adicione encapsulamento ao Jogador  - Adicionado encapsulamento na classe Jogador;  - Criados contrutores, getters e setters e toString na classe Jogador;  - Atualizada classe service para iniciar jogador com valores do construtor

--- src/model/Jogador.java
+++ src/model/Jogador.java
@@ -2,10 +2,49 @@ package model;
 
 public class Jogador {
 
-    String nome;
-    int idade;
-    int pontuacao;
-    int numeroTentivas;
+    private String nome;
+    private int idade;
+    private int pontuacao;
+    private int numeroTentivas;
+
+    public Jogador(String nome, int idade, int pontuacao, int numeroTentivas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.pontuacao = pontuacao;
+        this.numeroTentivas = numeroTentivas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public int getNumeroTentivas() {
+        return numeroTentivas;
+    }
+
+    public void setNumeroTentivas(int numeroTentivas) {
+        this.numeroTentivas = numeroTentivas;
+    }
 
     public void adicionaPontos(int pontos){
         pontuacao = pontuacao + pontos;
@@ -17,5 +56,15 @@ public class Jogador {
 
     public void adicionaTentativa(){
         numeroTentivas = numeroTentivas + 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Jogador{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", pontuacao=" + pontuacao +
+                ", numeroTentivas=" + numeroTentivas +
+                '}';
     }
 }
