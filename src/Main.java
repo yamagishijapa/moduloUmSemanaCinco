409856564c7688172cbaa3dc194b1a7ca90bc8e8
Yamagishi FMT <thiago_yamagishi@hotmail.com>
2024-02-12 00:21:45 -0300

    [M1S05] Ex 1 - Crie a Classe Jogador  - Adicionada classe jogador;  - Adicionada classe de controle do jogo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import service.JogoService;
+
 public class Main {
     public static void main(String[] args) {
-
+        JogoService jogoService =  new JogoService();
+        jogoService.iniciaJogo();
     }
 }
--- src/model/Jogador.java
+++ src/model/Jogador.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Jogador {
+
+    String nome;
+    int idade;
+    int pontuacao;
+    int numeroTentivas;
+
+    public void adicionaPontos(int pontos){
+        pontuacao = pontuacao + pontos;
+    }
+
+    public void perdePontos(int pontos){
+        pontuacao = pontuacao + pontos;
+    }
+
+    public void adicionaTentativa(){
+        numeroTentivas = numeroTentivas + 1;
+    }
 }
